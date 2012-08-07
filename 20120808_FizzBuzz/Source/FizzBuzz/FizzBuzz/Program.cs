017b03863b9effbe3ad2b60bcfc77f23581a6ba8
akihiro takada <umebayashi.akihiro@gmail.com>
2012-08-08 02:53:32 +0900

    FizzBuzz

---
+++ 20120808_FizzBuzz/Source/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 1; i <= 100; i++)
+			{
+				bool fizzbuzz = false;
+				if (i % 3 == 0)
+				{
+					Console.Write("Fizz");
+					fizzbuzz = true;
+				}
+				if (i % 5 == 0)
+				{
+					Console.Write("Buzz");
+					fizzbuzz = true;
+				}
+				if (!fizzbuzz)
+				{
+					Console.Write(i);
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
---
+++ 20120808_FizzBuzz/Source/FizzBuzz/FizzBuzzFS/Program.fs
@@ -0,0 +1,15 @@
+﻿// F# の詳細については、http://fsharp.net を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+
+[<EntryPoint>]
+let main argv = 
+    for i in 1 .. 100 do
+        if i % 15 = 0 then
+            printfn "FizzBuzz"
+        elif i % 3 = 0 then
+            printfn "Fizz"
+        elif i % 5 = 0 then
+            printfn "Buzz"
+        else
+            printfn "%d" i
+    0 
